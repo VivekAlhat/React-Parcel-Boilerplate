be388a47022bf31d76f9a3fc2f03a94812903d92
VivekAlhat <vivekalhat14@outlook.com>
2021-07-07 20:51:14 +0530

    Create & Test Boilerplate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Hello from "./components/Hello";
+
+const App = () => {
+    return (
+        <React.Fragment>
+            <Hello />
+        </React.Fragment>
+    );
+};
+
+export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,7 @@
+import { Container } from "@material-ui/core";
+
+const Hello = () => {
+    return <Container>Hello World!</Container>;
+};
+
+export default Hello;
